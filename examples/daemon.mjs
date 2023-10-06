08980f7b5a7066d9806390471483397c6fb0043e
vilicvane <vilicvane@live.com>
2023-10-07 00:32:49 +0800

    Setup sooner

--- examples/daemon.mjs
+++ examples/daemon.mjs
@@ -1,12 +1,12 @@
 import {StartupRun} from 'startup-run';
 
-setInterval(() => {
-  console.info(Date.now());
-}, 1000);
-
 const run = StartupRun.create('daemon-script');
 
 run.setup({
   enable: process.argv.includes('--enable'),
   disable: process.argv.includes('--disable'),
 });
+
+setInterval(() => {
+  console.info(Date.now());
+}, 1000);
